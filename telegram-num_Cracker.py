1dfc2bb85188c48ed923a595c5a1ff02fc6bac75
hacker X <141136594+hackerstech@users.noreply.github.com>
2026-07-09 14:28:20 +0530

    Update telegram-num_Cracker.py

--- telegram-num_Cracker.py
+++ telegram-num_Cracker.py
@@ -1,5 +1,8 @@
 import os,time,random
 l=[1,2,3,4,5,6,7,8,9,0]
+fol=open('filex.txt','a')
+os.system('touch filex.txt')
+
 print('starting .... made by hackerZX anon @kxzen_xx')
 while True:
     tar=random.choices(l,k=9)
@@ -8,6 +11,8 @@ while True:
     target=str(rnd)+str(targ)
     print(target)
     time.sleep(1.3)
+    fol.write(target)
+    fol.close()
     os.system(f'xdg-open tg://resolve?phone=91{target}')
 
 
